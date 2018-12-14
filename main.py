fea4cc2c1187acb4e3ea365884688a5a7d97281d
Anthony Thompson <34thompsonas@cua.edu>
2018-12-13 21:34:40 -0500

    Preserve username and email input even when invalid, added content to welcome page

--- main.py
+++ main.py
@@ -1,68 +1,78 @@
 from flask import Flask, request, redirect, render_template
 import cgi
+import re
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
+##
 @app.route("/")
 def index():
 	
-   ##Regenerates Username if previous Username was valid
+    ##Regenerates Username
     if request.args.get("form_name"):
         user_name1 =  request.args.get("form_name")
     else:
         user_name1 = ''
     ##----------
 
-    ##Getting any error values from previous form submission
+    ##Regenerates email
+    if request.args.get("form_email"):
+        e_mail1 =  request.args.get("form_email")
+    else:
+        e_mail1 = ''
+    ##----------
+
+    ##Extract error values from previous form submission if they exist
     user_name_error1 = request.args.get("user_name_error")
     pass_word_error1 = request.args.get("pass_word_error")
     vpass_word_error1 = request.args.get("vpass_word_error")
     e_mail_error1 = request.args.get("e_mail_error")
-    return render_template('signup.html', form_name=user_name1, user_name_error=user_name_error1 and cgi.escape(user_name_error1, quote=True),
+    return render_template('index.html', form_name=user_name1, form_email=e_mail1, 
+    user_name_error=user_name_error1 and cgi.escape(user_name_error1, quote=True),
     pass_word_error=pass_word_error1 and cgi.escape(pass_word_error1, quote=True),
     vpass_word_error=vpass_word_error1 and cgi.escape(vpass_word_error1, quote=True),
     e_mail_error=e_mail_error1 and cgi.escape(e_mail_error1, quote=True))
     ##----------
 
-# @app.route("/welcome", methods=['POST'])
-# def welcome_page():
-#     render_template('welcome.html', success='Welcome')
 
+##Extract form data
 @app.route("/submit", methods=['POST'])
 def form_submission():
-    # look inside the request to figure out what the user typed
     form_error = False
     user_name = request.form['user_name']
     pass_word =	request.form['pass_word']
     vpass_word = request.form['vpass_word']
     e_mail = request.form['e_mail']
-    error_list=['None','None', 'None', 'None', user_name]
+    cgi.escape(e_mail, quote=True)
+    cgi.escape(user_name, quote=True)
+    error_list=['None','None', 'None', 'None', user_name, e_mail]
+##-------------
 
     ##username validation
     if not user_name:
         error_list[0] = "Please enter a valid username"
-        error_list[4] = ""
-        user_name='None'        
+        # error_list[4] = ""
+        # user_name='None'        
         form_error=True
 
     if " " in user_name:
         error_list[0] = "Username must not contain spaces"
-        error_list[4] = ""
-        user_name='None'
+        # error_list[4] = ""
+        # user_name='None'
         form_error=True
 
     if len(user_name)<3 and len(user_name)>=1:
         error_list[0] = "Username too short, must be between 3 and 20 letters"
-        error_list[4] = ""
-        user_name='None'
+        # error_list[4] = ""
+        # user_name='None'
         form_error=True
 
     if len(user_name)>20:
         error_list[0] = "Username too long, must be between 3 and 20 letters"
-        error_list[4] = ""
-        user_name='None'
+        # error_list[4] = ""
+        # user_name='None'
         form_error=True
     ##-----------------
 
@@ -96,15 +106,15 @@ def form_submission():
         elength=len(e_mail)
         if (atcount != 1) or (pcount !=1) or (scount != 0) or (elength<3) or (elength>20):
             error_list[3] = "Invalid email address"
+            e_mail='None'
             form_error=True
     ##-------------
 
 
 
-    #form render or redirect
+    #Page render or redirect
     if form_error ==True:	
-    	return redirect("/?user_name_error={}&pass_word_error={}&vpass_word_error={}&e_mail_error={}&form_name={}".format(*error_list))
+    	return redirect("/?user_name_error={}&pass_word_error={}&vpass_word_error={}&e_mail_error={}&form_name={}&form_email={}".format(*error_list))
     else:
-    	#return render_template('signup.html', success='Welcome')
-        return render_template('welcome.html', success='Welcome')
+        return render_template('welcome.html', success='Welcome',username=user_name)
 app.run()
